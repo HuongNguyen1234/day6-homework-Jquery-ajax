4330f8b4e9ba4410b5ddf3be443c780b09d737fc
huongnguye1234 <huongnguyenthihuong1234@gmail.com>
2020-03-04 16:48:24 +0700

    done

--- app.js
+++ app.js
@@ -5,29 +5,30 @@ $(document).ready(function() {
         "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json?fbclid=IwAR2Z_oZyZt_QihXG0wJLG0E19ihHG_jbayxd2dQOOpVVRUPtUQULOQujuz0",
       success: function(result) {
         let questions = JSON.parse(result);
-        let quote =
+        let quotes =
           questions.quotes[
             Math.floor(Math.random() * questions.quotes.length) + 0
-          ].quote;
-        $("span").html(quote);
-        var letters = "0123456789ABCDEF";
-        var colors = "#";
-        for (var i = 0; i < 6; i++) {
+          ];
+        $("span").html(quotes.quote);
+        $("p").html("- " + quotes.author);
+        let letters = "0123456789ABCDEF";
+        let colors = "#";
+        for (let i = 0; i < 6; i++) {
           colors += letters[Math.floor(Math.random() * 16)];
         }
         console.log(colors);
-        $("#text-color").css({
-          color: "red",
+        $(".text-color").css({
+          color: colors,
           transition: "all 1s ease 0s"
         });
-        // $("body").css({
-        //   color: "red",
-        //   transition: "all 1s ease 0s"
-        // });
-        // $("#bg-color").css({
-        //   color: "red",
-        //   transition: "all 1s ease 0s"
-        // });
+        $("body").css({
+          "background-color": colors,
+          transition: "all 1s ease 0s"
+        });
+        $(".bg-color").css({
+          "background-color": colors,
+          transition: "all 1s ease 0s"
+        });
       }
     });
   });
